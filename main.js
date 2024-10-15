6af443501acbcbed1ef74cb078cd7c57a0a18ebe
7lostk <gsatria129@gmail.com>
2024-10-15 21:31:13 +0700

    sixth commit

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 (function () {
     "use strict";
 
-    /**
-     * Easy selector helper function
-     */
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
@@ -13,9 +10,6 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
     const on = (type, el, listener, all = false) => {
         let selectEl = select(el, all)
         if (selectEl) {
@@ -27,16 +21,10 @@
         }
     }
 
-    /**
-     * Easy on scroll event listener 
-     */
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
 
-    /**
-     * Navbar links active state on scroll
-     */
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
         let position = window.scrollY + 600
@@ -54,9 +42,6 @@
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
 
-    /**
-     * Scrolls to an element with header offset
-     */
     const scrollto = (el) => {
         let elementPos = select(el).offsetTop
         window.scrollTo({
@@ -65,9 +50,6 @@
         })
     }
 
-    /**
-     * Back to top button
-     */
     let backtotop = select('.back-to-top')
     if (backtotop) {
         const toggleBacktotop = () => {
@@ -81,18 +63,12 @@
         onscroll(document, toggleBacktotop)
     }
 
-    /**
-     * Mobile nav toggle
-     */
     on('click', '.mobile-nav-toggle', function (e) {
         select('body').classList.toggle('mobile-nav-active')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
     })
 
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
     on('click', '.scrollto', function (e) {
         if (select(this.hash)) {
             e.preventDefault()
@@ -108,9 +84,6 @@
         }
     }, true)
 
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
     window.addEventListener('load', () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
@@ -118,9 +91,7 @@
             }
         }
     });
-    /**
-     * Animation on scroll
-     */
+
     window.addEventListener('load', () => {
         AOS.init({
             duration: 1000,
@@ -130,9 +101,6 @@
         })
     });
 
-    /**
-     * Initiate Pure Counter 
-     */
     new PureCounter();
 
 })()
